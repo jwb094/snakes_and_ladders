5b04a5e23d4159bd02d545fa7247d6c71f445021
Josh Bello <Jb230913@live.com>
2022-03-22 19:53:13 +0000

    added logic to whoWon(),PlayerTurn(),ComputerTurn()

--- js/app.js
+++ js/app.js
@@ -4,13 +4,16 @@ computerturn = 1,
 playerposition = 1,
 computerposition,
 landonladder = false,
-landonsnakes = false;
-displaySteps();
+landonsnakes = false,
+ladders = [],
+snakes = [];
+
+displayStepsandCounters();
 
 /**
  * Display Numbers on Steps
  */
-function displaySteps(){
+function displayStepsandCounters(){
     let Steps =document.querySelectorAll('.box');
     Steps.forEach((step,i,a) => {
         step.innerHTML = `<h6>${100 - i}</h6>`;      
@@ -20,22 +23,66 @@ function displaySteps(){
 document.getElementById('dice').addEventListener("click",playerTurn);
 
 
-
+/**
+ * playerTurn()
+ * 
+ */
 function playerTurn(){
 
-    playerposition =  diceRoll(playerposition);
+
+    let dice =  diceRoll(playerposition);
+    playerposition = playerposition + dice;
+    positionMove();
+    checkForSnakesAndLadders();
+    WhosWon();
+    positionSwitch();
 }
 
 function computerTurn(){
-    computerturn = diceRoll(computerturn);
+    let dice = diceRoll(computerturn);
+    computerturn = computerturn + dice;
+    positionMove();
+    checkForSnakesAndLadders();
+    WhosWon();
+    positionSwitch();
 }
 
-function checkForSnakesAndLadders(){}
 
+function positionMove(){
 
+}
 
+function checkForSnakesAndLadders(){
+
+}
+
+function positionSwitch(){
+    
+    if (playerturn) {
+        playerturn = false;
+        computerturn = true;
+    } else {
+        playerturn = true;
+        computerturn = false;
+    }
+}
+
+/**
+ * diceRoll()
+ * @param {*} player_position 
+ * @returns  int between 1 and 6 
+ */
 function diceRoll(player_position){
-    let player_position = Math.floor(Math.random() * 6) + 1;
+     player_position = Math.floor(Math.random() * 6) + 1;
+     //console.log(player_position);
     return player_position;
 }
-function WhosWon(){}
+function WhosWon(){
+
+    return winner = playerposition === total ?  'Player 1 wins':
+                        computerposition === total ? 'CPU wins': 
+                         'No one winning';
+    
+
+
+}
