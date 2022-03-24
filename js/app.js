308d8e5e623f7ad42e5c24074bcd1cc3468ade62
Josh Bello <Jb230913@live.com>
2022-03-24 11:55:53 +0000

    WIP on positionMove() and adding Ladders and snakes lines with css instead of imgs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-const Target = 100;
+var target = 100;
 let playerturn =true,
-computerturn = 1,
+computerturn = true,
 playerposition = 1,
 computerposition,
 landonladder = false,
@@ -16,8 +16,16 @@ displayStepsandCounters();
 function displayStepsandCounters(){
     let Steps =document.querySelectorAll('.box');
     Steps.forEach((step,i,a) => {
-        step.innerHTML = `<h6>${100 - i}</h6>`;      
+        console.log(i);
+        step.innerHTML = `<h6>${100 - i}</h6>`;     
+        if(i == 99){
+            step.innerHTML =`<div id="P1"><p>P1</p></div><div id="CPU">CPU</div>`;
+        } 
     });
+    const thirdLink = document.querySelector(".box:nth-child(3)");
+   
+
+
 }
 
 document.getElementById('dice').addEventListener("click",playerTurn);
@@ -29,10 +37,11 @@ document.getElementById('dice').addEventListener("click",playerTurn);
  */
 function playerTurn(){
 
-
+    let prev_position = playerposition;
     let dice =  diceRoll(playerposition);
     playerposition = playerposition + dice;
-    positionMove();
+    console.log(`previous Prev is ${prev_position} new position is ${playerposition}`);
+    positionMove("P1",playerposition);
     checkForSnakesAndLadders();
     WhosWon();
     positionSwitch();
@@ -48,12 +57,27 @@ function computerTurn(){
 }
 
 
-function positionMove(){
+function positionMove(player,NewPos){
+    //remove element
+    setInterval(function() {
+        document.getElementById('P1').remove()
+    },3000);
+    //create new element in new psoition
+    setInterval(function(){
+       let new_position =  document.createElement("div");
 
+       // and give it some content
+        const newContent = document.createTextNode("P1");
+
+        // add the text node to the newly created div
+        new_position.appendChild(newContent);
+
+    },3000)
 }
 
 function checkForSnakesAndLadders(){
-
+    const snakes = [];
+    const ladders = [];
 }
 
 function positionSwitch(){
@@ -79,10 +103,9 @@ function diceRoll(player_position){
 }
 function WhosWon(){
 
-    return winner = playerposition === total ?  'Player 1 wins':
-                        computerposition === total ? 'CPU wins': 
+    return winner = playerposition === target ?  'Player 1 wins':
+                        computerposition === target ? 'CPU wins': 
                          'No one winning';
     
-
 
 }
